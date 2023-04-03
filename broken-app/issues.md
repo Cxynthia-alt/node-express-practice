e70718aceb635c346466774fdc6b8197627c20e3
fran <jane08150020@163.com>
2023-04-02 21:16:07 -0700

    set up

---
+++ broken-app/issues.md
@@ -0,0 +1,5 @@
+# Broken App Issues
+
+1. app.listen lacks callback function
+2. The global variables should be declared with const keywords instead so they are scoped inside this app.js only
+3. npm not installed, along with express
---
+++ urls.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const fs = require('fs')
+const process = require('process');
+
+
+
+function saveURL(data, fileName) {
+  fs.writeFile(fileName, data, 'utf8', (err) => {
+    if (err) {
+      console.log("Error:", err)
+      process.kill(1)
+    }
+  })
+}
+
+
+function readFile(path) {
+  const fileContents = fs.readFileSync(path, 'utf-8')
+  const lines = fileContents.split('\n').filter(line => line.trim() !== " ")
+  const promises = lines.map(url => {
+    return axios.get(url)
+      .then(res => {
+        const fileName = new URL(`${url}`).hostname
+        saveURL(res.data, fileName)
+      })
+      .catch(error => {
+        console.log("Error", error)
+        return { url, error }
+      })
+  })
+  console.log(promises)
+  Promise.all(promises)
+    .then(results => {
+      results.forEach(result => {
+        if (!result) {
+          console.log(`Coudln't download ${result.url}`)
+        } else {
+          console.log(`Wrote to ${result.url}`)
+        }
+      })
+    })
+}
+readFile(process.argv[2])
